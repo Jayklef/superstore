7aa4fb60754bc89218b0274e4e6c061c1609351e
Jayklef <arhawhojerry@gmail.com>
2025-04-06 08:01:05 +0100

    project setup and DB config

---
+++ src/main/java/com/jayklef/superstore_app/SuperstoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.jayklef.superstore_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperstoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperstoreAppApplication.class, args);
+	}
+
+}
