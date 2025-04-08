c9ce522595ade1ebb10dde13a0e38ba2bc2fec47
Jayklef <arhawhojerry@gmail.com>
2025-04-08 19:49:00 +0100

    Product/Home controllers

---
+++ src/main/java/com/jayklef/superstore_app/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.jayklef.superstore_app.controller;
+
+import com.jayklef.superstore_app.entity.Admin;
+import com.jayklef.superstore_app.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    private ProductService productService;
+    @GetMapping({"/", "/home" })
+    public String homePage() {
+        return "homePage";
+    }
+
+    @GetMapping("/products")
+    public String productsPage(Model model) {
+        model.addAttribute("productList", productService.getAllProducts());
+        return "ProductPage";
+    }
+
+
+
+    @GetMapping("/contact")
+    public String contactPage() {
+        return "contactPage";
+    }
+
+    @GetMapping("/about")
+    public String aboutUs() {
+        return "aboutUs";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+        model.addAttribute("admin", new Admin());
+        return "Login";
+    }
+
+}
--- src/main/java/com/jayklef/superstore_app/controller/ProductController.java
+++ src/main/java/com/jayklef/superstore_app/controller/ProductController.java
@@ -5,15 +5,40 @@ import com.jayklef.superstore_app.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
 
     @Autowired
     ProductService productService;
-    @GetMapping("/products")
-    public String addProductPage() {
+    @GetMapping("/add/products")
+    public String addProduct() {
         return "addProduct";
+    }
+    @PostMapping("/add/products")
+    public String addProduct(Product product) {
+        productService.addProduct(product);
+        return "admin/home";
+    }
+    @GetMapping("/update/products/{id}")
+    public String updateProductPage(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "updateProduct";
+    }
+
+    @PostMapping("/update/product")
+    public String updateProduct(Product product) {
+        productService.updateProduct(product);
+        return "admin/home";
+    }
+
+    @DeleteMapping("/delete/products/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return "admin/home";
     }
 }
